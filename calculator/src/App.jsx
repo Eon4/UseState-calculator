693a91e147d12edf4201e46438889daba81f796c
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-17 11:23:45 +0200

    update

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [result, setResult] = useState(0)
+  const [valueOne, setvalueOne] = useState(0)
+  const [valueTwo, setvslueTwo] = useState(0)
+
+  const add = () => {
+    const res = valueOne + valueTwo
+    setResult(res)
+  }
+
 
   return (
     <>
